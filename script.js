b1b942d224120077df5fab31c1e280d47fa53968
Lculi <98343082+Lculi@users.noreply.github.com>
2022-01-24 09:26:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+/* function emailValidate(email) {
+	if(email.includes('@')) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function clickListener(event) {
+	event.preventDefault();
+	
+	let emailInput = document.querySelector('#email');
+	let messageInput = documet.querySelector('#message');
+	let emailText = emailInput.value;
+	let messageText = messageInput.value;
+	
+	console.log('email:', emailText, 'message:' messageText);
+	
+	if(emailValidate(emailText) !== true) {
+		alert('The email must contain @');
+		return false;
+	}
+	alert('Thanks for your message!')
+} 
+
+let submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', clickListener); 
+
+funtion clickListener() {
+	console.log('Button clicked');
+}
+
+submit.addEventListener('click', clickListener); */
+
